77523caccfe67e70430491321610178016aa1b47
Jee Soo Park <zmailzzz@gmail.com>
2020-02-13 01:29:05 +0900

    artist 추가...!!

--- SPCC_JSP_HW4.py
+++ SPCC_JSP_HW4.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
 
 # URL을 읽어서 HTML를 받아오고,
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908', headers=headers)
 
 # HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
 soup = BeautifulSoup(data.text, 'html.parser')
@@ -25,24 +26,25 @@ music_data = {
     'artist': ''
 }
 
-
 for music in musics:
     # movie 안에 a 가 있으면,
     a_tag = music.select_one('td.info > a.title.ellipsis')
 
-    if a_tag is not None:
-        title = a_tag.text.lstrip()
-        photo = music.select_one('td:nth-child(3) > a > img').get('src')
-        artist = music.select_one('img').attrs['src'].replace('//','')
-        print(rank,photo,title,artist)
+# if a_tag is not None:
+    title = a_tag.text.lstrip()
+    photo = music.select_one('td:nth-child(3) > a > img').get('src').replace('//', '')
+    # photo2 = music.select_one('img').attrs['src'].replace('//','')
+    artist = music.select_one('td.info > a.artist.ellipsis').text.lstrip()
+    print(rank, photo, title, artist)
 
-        music_data = {
-            'rank': rank,
-            'title': title,
-            'photo': photo,
-            'artist': artist
-        }
-        db.musics.insert_one(music_data)
+    music_data = {
+        'rank': rank,
+        'title': title,
+        'photo': photo,
+        'artist': artist
+    }
 
-        rank += 1
+    rank += 1
 
+
+    db.musics.insert_one(music_data)
