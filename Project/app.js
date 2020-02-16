12b2e3107378cbc9cbb9b65d0f769957c396b631
Jee Soo Park <zmailzzz@gmail.com>
2020-02-17 01:48:38 +0900

    경로 수정하였습니다 ( 매우뿌듯)

--- Project/app.js
+++ Project/app.js
@@ -10,16 +10,16 @@ app.get("/", function(req, res) {
 // [출처] [Node.js] express 로 웹서버 생성 + 정적파일(css, js 등) 설정|작성자 demonic
 
 // hello.all.js 참조
-app.use("/lib", express.static(__dirname + "/Project/lib/"));
+app.use("/lib", express.static(__dirname + "/lib/"));
 //
 // 구글 인증 테스트 페이지
 app.get("/auth/google", function(req, res) {
-  res.sendFile(__dirname + "/Project/auth/google.html");
+  res.sendFile(__dirname + "/auth/google.html");
 });
 
 // 구글 인증 콜백
 app.get("/auth/callbackGoogle", function(req, res) {
-  res.sendFile(__dirname + "/Project/auth/google.html");
+  res.sendFile(__dirname + "/auth/google.html");
 });
 //
 
